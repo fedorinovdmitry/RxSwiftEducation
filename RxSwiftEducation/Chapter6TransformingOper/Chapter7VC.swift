bcacd0799708fb697780dcb0e7c00edc330b69c6
Dmitry Fedorinov <fedorinov.dmitry@gmail.com>
2022-03-22 12:12:21 +0300

    Setup Menu sceen

---
+++ RxSwiftEducation/Chapter6TransformingOper/Chapter7VC.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter7VC.swift
+//  RxSwiftEducation
+//
+//  Created by Дмитрий Федоринов on 22.03.2022.
+//
+
+import UIKit
+import RxSwift
+
+class Chapter7VC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
